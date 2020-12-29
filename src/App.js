6f363ad1a0c12e18278afabda9be33afdc559483
Darshan <dhulswar591@gmail.com>
2020-12-29 12:24:42 +0530

    removed button outline on focus

--- src/App.js
+++ src/App.js
@@ -9,28 +9,60 @@ function App() {
           Screen
         </div>
         <div className="flex flex-row justify-evenly">
-          <button className="p-7 my-2 bg-white font-bold">7</button>
-          <button className="p-7 my-2 bg-white font-bold">8</button>
-          <button className="p-7 my-2 bg-white font-bold">9</button>
-          <button className="p-7 my-2 bg-yellow-500 font-bold">+</button>
+          <button className="p-7 my-2 bg-white font-bold border-none focus:outline-none">
+            7
+          </button>
+          <button className="p-7 my-2 bg-white font-bold focus:outline-none">
+            8
+          </button>
+          <button className="p-7 my-2 bg-white font-bold focus:outline-none">
+            9
+          </button>
+          <button className="p-7 my-2 bg-yellow-500 font-bold focus:outline-none">
+            +
+          </button>
         </div>
         <div className="flex flex-row justify-evenly">
-          <button className="p-7 my-2 bg-white font-bold">4</button>
-          <button className="p-7 my-2 bg-white font-bold">5</button>
-          <button className="p-7 my-2 bg-white font-bold">6</button>
-          <button className="p-7 my-2 bg-yellow-500 font-bold">-</button>
+          <button className="p-7 my-2 bg-white font-bold focus:outline-none">
+            4
+          </button>
+          <button className="p-7 my-2 bg-white font-bold focus:outline-none">
+            5
+          </button>
+          <button className="p-7 my-2 bg-white font-bold focus:outline-none">
+            6
+          </button>
+          <button className="p-7 my-2 bg-yellow-500 font-bold focus:outline-none">
+            -
+          </button>
         </div>
         <div className="flex flex-row justify-evenly">
-          <button className="p-7 my-2 bg-white font-bold">1</button>
-          <button className="p-7 my-2 bg-white font-bold">2</button>
-          <button className="p-7 my-2 bg-white font-bold">3</button>
-          <button className="p-7 my-2 bg-yellow-500 font-bold">*</button>
+          <button className="p-7 my-2 bg-white font-bold focus:outline-none">
+            1
+          </button>
+          <button className="p-7 my-2 bg-white font-bold focus:outline-none">
+            2
+          </button>
+          <button className="p-7 my-2 bg-white font-bold focus:outline-none">
+            3
+          </button>
+          <button className="p-7 my-2 bg-yellow-500 font-bold focus:outline-none">
+            *
+          </button>
         </div>
         <div className="flex flex-row justify-evenly">
-          <button className="p-7 my-2 bg-green-200 font-bold">C</button>
-          <button className="p-7 my-2 bg-white font-bold">0</button>
-          <button className="p-7 my-2 bg-yellow-500 font-bold">=</button>
-          <button className="p-7 my-2 bg-yellow-500 font-bold">/</button>
+          <button className="p-7 my-2 bg-green-200 font-bold focus:outline-none">
+            C
+          </button>
+          <button className="p-7 my-2 bg-white font-bold focus:outline-none">
+            0
+          </button>
+          <button className="p-7 my-2 bg-yellow-500 font-bold focus:outline-none">
+            =
+          </button>
+          <button className="p-7 my-2 bg-yellow-500 font-bold focus:outline-none">
+            /
+          </button>
         </div>
       </div>
     </div>
